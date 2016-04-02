98fd4ee0fece2ea259b56a9354d2cff79620c567
Prashant Lokhande <lprashant94@gmail.com>
2016-04-03 02:23:26 +0530

    Adding sequencial quick sort

--- quick sort/quick sort.py
+++ quick sort/quick sort.py
@@ -19,9 +19,35 @@ class MovieHandler( xml.sax.ContentHandler ):
       if self.CurrentData == "Number":
          self.Number = content
 
+class sorting:
+    def __init__(self,input):
+        self.input_list = input
+        self.length = len(input)
+        self.quicksort(0,self.length-1)
+    def partition(self,i,j):
+        pivot_place=i
+        pivot = self.input_list[i]
+        print(str(i),' ' ,str(j))
+        while i<j:
+            while pivot >= self.input_list[i] and i <j:
+                i+=1
+            while pivot < self.input_list[j]:
+                j-=1
+            if(i<j):
+                self.input_list[i],self.input_list[j]=self.input_list[j],self.input_list[i]
+        self.input_list[pivot_place],self.input_list[j] = self.input_list[j], self.input_list[pivot_place]
+        print(str(self.input_list))
+        return j;
+    
+    def quicksort(self,i,j):
+        m=0
+        if(i<j):
+            m = self.partition(i,j)
+            self.quicksort(i,m-1)
+            self.quicksort(m+1,j)
 
-def sort:
-    return 0
+        
+        return 0
 
 
 
@@ -33,3 +59,5 @@ parser.setContentHandler( Handler )
 parser.parse("test.xml")
 
 print(str(Handler.outputlist))
+s= sorting(Handler.outputlist)
+print(str(s.input_list))
